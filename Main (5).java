afb9787a91bd78b53c6d451acd5058c798ef8d9a
Yash-1812 <ykerkar61@gmail.com>
2025-08-09 15:42:00 +0530

    Method Overloading/Run time Polymorphism

---
+++ Main (5).java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String args[]){
+        MathUtils obj = new MathUtils();
+        System.out.println(obj.add(1 , 2));
+        System.out.println(obj.add(2.5 , 2.5));
+    }
+}
